e5c112a4e027a805275357a85b6da0fca2927252
Charlesmyke <charlesmyke07@gmail.com>
2022-10-22 16:01:16 -0700

    Debug task 3

--- 0x03-debugging/3-print_remaining_days.c
+++ 0x03-debugging/3-print_remaining_days.c
@@ -11,7 +11,6 @@
 
 void print_remaining_days(int month, int day, int year)
 {
-	/* checks if the year is a year or not */
 	if (year % 4 == 0 || (year % 400 == 0 && year % 100 == 0))
 	{
 		if (month > 2 && day >= 60)
