f600202367ee98c48f8a647bcd5d1de5a8c61413
Charlesmyke <charlesmyke07@gmail.com>
2022-10-22 16:12:21 -0700

    fxn task 8

--- 0x02-functions_nested_loops/8-24_hours.c
+++ 0x02-functions_nested_loops/8-24_hours.c
@@ -1,35 +1,24 @@
 #include "main.h"
 
 /**
- * jack_bauer - Prints all minutes in a day.
+ * jack_bauer - Prints every minute of the day
  *
- * Return: 0 if successful.
  */
 
 void jack_bauer(void)
 {
-	int i, j, k, m;
+	int hour, minute;
 
-	for (i = 0; i <= 2; i++)
+	for (hour = 0; hour <= 23; hour++)
 	{
-		for (j = 0; j <= 9; j++)
+		for (minute = 0; minute <= 59; minute++)
 		{
-			for (k = 0; k <= 5; k++)
-			{
-				for (m = 0; k <= 9; m++)
-				{
-					if (i == 2 && j > 3)
-					{break;
-					}
-
-					_putchar(i + '0');
-					_putchar(j + '0');
-					_putchar(':');
-					_putchar(k + '0');
-					_putchar(m + '0');
-					_putchar('\n');
-				}
-			}
+			_putchar((hour / 10) + '0');
+			_putchar((hour % 10) + '0');
+			_putchar(':');
+			_putchar((minute / 10) + '0');
+			_putchar((minute % 10) + '0');
+			_putchar('\n');
 		}
 	}
 }
