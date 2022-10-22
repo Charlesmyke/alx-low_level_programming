57e327e744f944e9aaecc38f11b06ed460697a20
Charlesmyke <charlesmyke07@gmail.com>
2022-10-22 16:24:43 -0700

    fxn task 10

---
+++ 0x02-functions_nested_loops/10-add.c
@@ -0,0 +1,14 @@
+#include "main.h"
+
+/**
+ * add - adds two integers
+ * @num1: first integer to be added
+ * @num2: second integer to be added
+ *
+ * Return: addition result
+ */
+
+int add(int num1, int num2)
+{
+	return (num1 + num2);
+}
