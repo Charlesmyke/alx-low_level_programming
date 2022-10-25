932bb5d0b9f12a30f994c1032e7f5c07c02ce36f
Charlesmyke <charlesmyke07@gmail.com>
2022-10-24 17:31:32 -0700

    P&A task 9

---
+++ 0x05-pointers_arrays_strings/9-strcpy.c
@@ -0,0 +1,24 @@
+#include "main.h"
+#include <stdio.h>
+
+/**
+ * _strcpy - copies the string pointed to by src.
+ * @dest: char to check
+ * @src: source code
+ *
+ * Return: 0 is success
+ */
+
+char *_strcpy(char *dest, char *src)
+{
+	int count = 0;
+
+	while (count >= 0)
+	{
+		*(dest + count) = *(src + count);
+		if (*(src + count) == '\0')
+			break;
+		count++;
+	}
+	return (dest);
+}
