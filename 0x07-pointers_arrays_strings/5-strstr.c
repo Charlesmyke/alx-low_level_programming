d3964a7d7d0655fd5a5a80a7566ada73205d6ce3
Charlesmyke <charlesmyke07@gmail.com>
2022-10-31 15:27:38 -0700

    Even more P&A task 5

---
+++ 0x07-pointers_arrays_strings/5-strstr.c
@@ -0,0 +1,33 @@
+#include "main.h"
+
+/**
+ * _strstr - function that locates a substring
+ * @needle: finds first occurrence in substring
+ * @haystack: longer string to search
+ *
+ * Return: pointer to beginning of substring
+ */
+
+char *_strstr(char *haystack, char *needle)
+{
+	unsigned int i = 0, j = 0;
+
+	while (haystack[i])
+	{
+		while (needle[j] && (haystack[i] == needle[0]))
+		{
+			if (haystack[i + j] == needle[j])
+				j++;
+			else
+				break;
+		}
+		if (needle[j])
+		{
+			i++;
+			j = 0;
+		}
+		else
+			return (haystack + i);
+	}
+	return (0);
+}
