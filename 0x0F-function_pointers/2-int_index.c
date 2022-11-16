a134b063046736caf4aec4412ee6139d63568dbc
Charlesmyke <charlesmyke07@gmail.com>
2022-11-16 15:01:38 -0800

    Fxn pointers task 2

---
+++ 0x0F-function_pointers/2-int_index.c
@@ -0,0 +1,31 @@
+#include "function_pointers.h"
+
+/**
+ * int_index - function that searches for integer
+ * @array: array to be used
+ * @size: size of array
+ * @cmp: pointer to function
+ *
+ * Return: void
+ */
+
+int int_index(int *array, int size, int (*cmp)(int))
+{
+	int i = 0;
+
+	if (size > 0)
+	{
+		if (array != NULL && cmp != NULL)
+		{
+			while (i < size)
+			{
+				if (cmp(array[i]))
+					return (i);
+
+				i++;
+			}
+		}
+	}
+
+	return (-1);
+}
